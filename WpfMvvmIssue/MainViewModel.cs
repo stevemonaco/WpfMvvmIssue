8133eb7da2a0efd5e7b8b99060d476ecf9ce1e7b
Stephen Monaco <stevemonaco@hotmail.com>
2021-11-11 17:50:06 -0500

    Add project files.

---
+++ WpfMvvmIssue/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace WpfMvvmIssue;
+
+[ObservableObject]
+public partial class MainViewModel
+{
+    [ObservableProperty]
+    private string? _name;
+}
---
+++ WpfMvvmIssue/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace WpfMvvmIssue;
+
+[ObservableObject]
+public partial class MainWindow : Window
+{
+    [ObservableProperty]
+    private string? _name;
+
+    public MainWindow()
+    {
+        _name = "Testing456";
+
+        InitializeComponent();
+        //DataContext = new MainViewModel() { Name = "Testing456" };
+        DataContext = this;
+    }
+}
